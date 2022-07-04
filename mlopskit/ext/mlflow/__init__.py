daa661eb52f52e90b4973cd9852d63fddc37015f
leepand <leepand@leepanddeMacBook-Pro.local>
2022-07-04 15:43:57 +0800

    update mlopskit

--- mlopskit/ext/mlflow/__init__.py
+++ mlopskit/ext/mlflow/__init__.py
@@ -1,7 +1,7 @@
-"""``modelkit.extras`` provides functionality such as datasets/models and extensions using mlflow client.
+"""``mlopskit.extras`` provides functionality such as datasets/models and extensions using mlflow client.
 """
-from modelkit.ext.mlflow.model_tracking.model_tracking import ModelTracking
-from modelkit.ext.mlflow.model_registry.model_deploy import ModelDeploy
+from mlopskit.ext.mlflow.model_tracking.model_tracking import ModelTracking
+from mlopskit.ext.mlflow.model_registry.model_deploy import ModelDeploy
 
 __all__=[
     'ModelTracking',
