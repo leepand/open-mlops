941615a1285f1baed5fc75776df26d25956925e9
leepand <leepand@leepanddeMacBook-Pro.local>
2022-07-18 11:57:26 +0800

    add repokit

---
+++ mlopskit/ext/repokit/repos/__init__.py
@@ -0,0 +1,6 @@
+from .memory import MemoryRepo
+#from .mongo import MongoRepo
+from .sqlalchemy import SQLRepo
+from .rest import RESTRepo
+from .csv import CSVFileRepo
+from .json import JSONDirectoryRepo
